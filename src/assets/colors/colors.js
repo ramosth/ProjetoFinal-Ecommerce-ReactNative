443cc9e132fc883217c1101687e9d707590a958e
Thamires Ramos <thamiresramos014@gmail.com>
2021-07-13 14:03:11 -0300

    pasta components

--- src/assets/colors/colors.js
+++ src/assets/colors/colors.js
@@ -1,16 +1,6 @@
 const colors = {
-  primaryColor: '#FE554A',
-  gradient: '#F9881F',
-  accent: '#0B735F',
-  textDark: '#2A3037',
-  textMedium: '#C6C9CC',
-  textLight: '#DFE2E5',
-  white: '#FCFCFC',
-  background: '#F7F7FB',
-  backgroundColor: '#EBF5FB',
-  inputColor: '#DFE2E5',
-  inputTextColor: '#AAACAE',
-  black: '#000',
+  azul: '#010035',
+  laranja: '#FF6E4E',
 };
 
 export default colors;
