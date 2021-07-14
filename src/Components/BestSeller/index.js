1422cb5851bc082c9646db1c05bbd22f504180c0
Cristiny C. Boêta <cristinybt20@gmail.com>
2021-07-14 16:59:40 -0300

    16:59

--- src/Components/BestSeller/index.js
+++ src/Components/BestSeller/index.js
@@ -3,7 +3,6 @@ import {TouchableOpacity, View, Text} from 'react-native';
 import CardProduto from './CardProduto';
 import style from './style';
 
-<<<<<<< HEAD
 const BestSeller = ({navigation}) => {
     return (
         <View style={style.container}>
@@ -24,27 +23,5 @@ const BestSeller = ({navigation}) => {
         </View>
     )
 }
-=======
-const BestSeller = () => {
-  return (
-    <View style={style.container}>
-      <View style={style.titulos}>
-        <Text style={style.title}>Best Seller</Text>
-        <TouchableOpacity>
-          <Text style={style.seeMore}>see more</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={style.row}>
-        <CardProduto />
-        <CardProduto />
-      </View>
-      <View style={style.row}>
-        <CardProduto />
-        <CardProduto />
-      </View>
-    </View>
-  );
-};
->>>>>>> aa3ae5f2f608d5885856eb088d34aea8a79a6c10
 
 export default BestSeller;
