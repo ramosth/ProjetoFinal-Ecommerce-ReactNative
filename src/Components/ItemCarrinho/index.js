fb661a8f0a9fec8586fe7437d7e12d86e296625f
Thamires Ramos <thamiresramos014@gmail.com>
2021-07-16 17:45:13 -0300

    ajuste nos botoes de quantidade

--- src/Components/ItemCarrinho/index.js
+++ src/Components/ItemCarrinho/index.js
@@ -2,7 +2,7 @@
 import React, {useState} from 'react';
 import colors from '../../assets/colors/colors';
 import {styles} from './styles';
-import {View, Image, Text} from 'react-native';
+import {View, Image, Text, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Button from '../Button';
 import api from '../../services/Api/api';
@@ -42,19 +42,13 @@ export default function ItemCarrinho(props) {
         <Text style={styles.textoItemPreco}>{props.preco}</Text>
       </View>
       <View style={styles.btnQuantidade}>
-        <Button
-          titulo="-"
-          buttonStyles={styles.buttonQuantidade}
-          tituloStyles={styles.tituloQuantidade}
-          onPress={decremento}
-        />
+        <TouchableOpacity style={styles.buttonQuantidade} onPress={decremento}>
+          <Text style={styles.tituloQuantidade}>-</Text>
+        </TouchableOpacity>
         <Text style={styles.textoQuantidade}>{contador}</Text>
-        <Button
-          titulo="+"
-          buttonStyles={styles.buttonQuantidade}
-          tituloStyles={styles.tituloQuantidade}
-          onPress={incremento}
-        />
+        <TouchableOpacity style={styles.buttonQuantidade} onPress={incremento}>
+          <Text style={styles.tituloQuantidade}>+</Text>
+        </TouchableOpacity>
       </View>
       <Ionicons
         name="md-trash-outline"
